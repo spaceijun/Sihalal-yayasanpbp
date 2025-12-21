3ae2faaae70f2743f8bd7dec7f35a1bfd1e53b3d
ijunnn <faizunaziz53@gmail.com>
2025-12-21 19:46:03 +0700

    edit env.example for sanctum

--- config/sanctum.php
+++ config/sanctum.php
@@ -16,12 +16,10 @@ return [
     */
 
     'stateful' => explode(',', env('SANCTUM_STATEFUL_DOMAINS', sprintf(
-        '%s%s%s',
+        '%s%s',
         'localhost,localhost:3000,127.0.0.1,127.0.0.1:8000,::1',
-        env('APP_URL') ? ',' . parse_url(env('APP_URL'), PHP_URL_HOST) : '',
-        ',sihalal-yayasanpbp.test'
+        env('APP_URL') ? ',' . parse_url(env('APP_URL'), PHP_URL_HOST) : ''
     ))),
-
     /*
     |--------------------------------------------------------------------------
     | Sanctum Guards

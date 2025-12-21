14c1bbb72b7a6e7e0603ac036728c1472af1e689
ijunnn <faizunaziz53@gmail.com>
2025-12-21 19:28:54 +0700

    edit env.example for sanctum

--- app/Models/User.php
+++ app/Models/User.php
@@ -9,12 +9,13 @@ use Illuminate\Auth\Passwords\CanResetPassword;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
 use Spatie\Permission\Traits\HasRoles;
 
 class User extends Authenticatable
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
-    use HasFactory, Notifiable, CanResetPassword, HasRoles;
+    use HasApiTokens, HasFactory, Notifiable, CanResetPassword, HasRoles;
 
     /**
      * The attributes that are mass assignable.

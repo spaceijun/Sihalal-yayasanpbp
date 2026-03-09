1c0765f482ec96f454e1d7ad245a94d3690fb5ef
ijunnn <faizunaziz53@gmail.com>
2026-03-09 20:38:36 +0700

    Fix storage datalapangan API

--- app/Http/Controllers/Api/Enumerator/DataLapanganEnumController.php
+++ app/Http/Controllers/Api/Enumerator/DataLapanganEnumController.php
@@ -14,6 +14,7 @@ class DataLapanganEnumController extends Controller
 {
     const STATUS_LIST = [
         'PENDING',
+        'REVISI',
         'TERVERIFIKASI',
         'PROGRESS OSS',
         'PROGRESS SIHALAL',

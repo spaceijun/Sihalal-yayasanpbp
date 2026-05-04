85e5c25b3f582f37cb9a34326f413631fd817656
ijunnn <faizunaziz53@gmail.com>
2026-05-04 21:44:18 +0700

    Update Pengajuan datalapangan

---
+++ database/migrations/2026_05_04_214247_add_has_nib_to_data_lapangans_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('data_lapangans', function (Blueprint $table) {
+            $table->boolean('has_nib')->default(false)->after('file_oss');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('data_lapangans', function (Blueprint $table) {
+            $table->dropColumn('has_nib');
+        });
+    }
+};

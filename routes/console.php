7cf858d7fec458fa04a250b82c1b1c07e4371174
ijunnn <faizunaziz53@gmail.com>
2026-05-14 00:03:54 +0700

    FIX Upload data enum API

--- routes/console.php
+++ routes/console.php
@@ -8,6 +8,6 @@ Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
 
-Schedule::command('data-lapangan:clean-locks')->everyFiveMinutes();
+Schedule::command('data-lapangan:clean-locks')->everyMinute();
 // Schedule::command('enumerator:check-activity')->monthlyOn(25, '00:00');
-Schedule::command('status:update-pembayaran')->daily();
+Schedule::command('status:update-pembayaran')->everyMinute();

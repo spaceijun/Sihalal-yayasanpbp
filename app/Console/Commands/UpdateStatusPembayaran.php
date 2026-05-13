95041e2a7555059755269ce1ed3f75106f658d28
ijunnn <faizunaziz53@gmail.com>
2026-05-14 00:13:38 +0700

    FIX Upload data enum API

--- app/Console/Commands/UpdateStatusPembayaran.php
+++ app/Console/Commands/UpdateStatusPembayaran.php
@@ -9,17 +9,14 @@ use Illuminate\Console\Command;
 class UpdateStatusPembayaran extends Command
 {
     protected $signature = 'status:update-pembayaran';
-    protected $description = 'Update status_pembayaran menjadi Pengajuan jika sudah 2 hari setelah TERBIT SH';
-
+    protected $description = 'Update status_pembayaran menjadi PENGAJUAN jika sudah 2 hari setelah TERBIT SH';
     public function handle()
     {
         $duaHariLalu = Carbon::now()->subDays(2);
-
         $updated = DataLapangan::where('status', 'TERBIT SH')
-            ->where('status_pembayaran', '!=', 'PENGAJUAN')
+            ->where('status_pembayaran', 'PENDING')
             ->where('tanggal_verifikasi', '<=', $duaHariLalu)
             ->update(['status_pembayaran' => 'PENGAJUAN']);
-
         $this->info("Updated {$updated} records.");
     }
 }
